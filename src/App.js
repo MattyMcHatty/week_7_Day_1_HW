9a7d61a6de4dbd030253f8cf901e102f377f6635
MattyMcHatty <54686014+MattyMcHatty@users.noreply.github.com>
2022-10-10 20:57:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {name: "Buy shopping", priority: true},
+    {name: "Clean bathroom", priority: false},
+    {name: "Car's MOT", priority: true}
+  ])
+
+  const [newTask, setNewTask] = useState("");
+
+  const taskNodes = tasks.map((task, index) =>{
+    return(
+    <li key = {index} className={task.priority ? "is-priority" : "isnt-priority"}>
+      <span>{task.name} </span>
+      {
+        task.priority ? <span className = "is-priority">High</span> :
+        <span className = "isnt-priority">Low</span>
+      }
+      
+    </li>   
+    )
+  })
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const handleOptionChange = (changeEvent) => {
+    this.setState({
+      selectedOption: changeEvent.target.value
+    });
+  }
+
+  const saveNewTask = (event) => {
+    event.preventDefault();
+    const copyTasks = [...tasks];
+    copyTasks.push({name: newTask, priority: this.state.selectedOption});
+    setTasks(copyTasks);
+    setNewTask("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List</h1>
+      <hr></hr>
+      
+      <form onSubmit={saveNewTask}>
+        <input id = "new-task" type = "text" value={newTask} onChange={handleTaskInput}/>
+        <label for = "high">High</label>
+        <input type = "radio" id = "high" checked={this.state.selectedOption === true} onChange={handleOptionChange}></input>
+        <label for = "low">Low</label>
+        <input type = "radio" id = "low"checked={this.state.selectedOption === false} onChange={handleOptionChange}></input>
+        <input type="submit" value = "Save Task" />
+      </form>
+
+      <ul>
+        {taskNodes}
+      </ul>
+      
     </div>
   );
 }
